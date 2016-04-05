a97d034575cd9522fbae714ad1bd2d9faef81091
John the Scott <jmscott@setspace.com>
2016-04-05 16:30:26 -0500

    tweak

--- godfw-hoq-20160405/flow.go
+++ godfw-hoq-20160405/flow.go
@@ -2,8 +2,7 @@
 
 type flow struct {
 
-	//  request a new flow from this channel,   // HL
-	//  reading reply on sent side-channel      // HL
+	//  request a new flow from this channel, reading reply on sent side-channel   // HL
 	next chan flow_chan
 
 	//  channel is closed when all exec()s make no further progress // HL
@@ -12,11 +11,10 @@ type flow struct {
 	//  the whole line of input with trailing new line removed   // HL
 	line string
 
-	//  tab separated fields split out from the line read from   // HL
-	//  standard input    // HL
+	//  tab separated fields split out from the line read from standard input   // HL
 	fields []string
 
-	//  count of go routines still resolving qualifications  // HL
+	//  count of go routines/operators still resolving qualifications  // HL
 	confluent_count int
 }
 type flow_chan chan *flow
